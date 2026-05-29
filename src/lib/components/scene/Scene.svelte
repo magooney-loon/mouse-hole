6b5b107e078c5df39177d1d65b5a11b55728a800
Magooney <magooney-loon@proton.me>
2026-05-29 20:09:51 +0200

    init model

--- src/lib/components/scene/Scene.svelte
+++ src/lib/components/scene/Scene.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
-	import { GLTF, interactivity } from '@threlte/extras';
+	import { GLTF, OrbitControls, interactivity } from '@threlte/extras';
 
 	interactivity();
+
+	const DEV_ORBIT = 1; // set to 0 to disable
 </script>
 
-<T.PerspectiveCamera position={[0, 10, 10]} fov={50} lookAt={[0, 0, 0]} makeDefault />
+<T.PerspectiveCamera position={[0, 35, 0]} fov={60} makeDefault>
+	{#if DEV_ORBIT}
+		<OrbitControls />
+	{/if}
+</T.PerspectiveCamera>
 
 <T.DirectionalLight position={[5, 10, 5]} intensity={1} />
 <T.AmbientLight intensity={0.6} />
 
-<GLTF url="/house/scene.gltf" position={[-0.08, 9, 11]} />
+<GLTF url="/house.glb" position={[-2.5, 9, 2.5]} />
